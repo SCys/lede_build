b085bfa4409a6c3b695f9042583fe07e9024dd3d
SCys <me@iscys.com>
2021-07-15 10:25:18 +0800

    + 增加 zram 相关

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
