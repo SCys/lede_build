0a36eb93fd97ebaffa9e5f2a945e1eeac49aba4a
SCys <me@iscys.com>
2021-07-20 21:07:43 +0800

    + 合并上游的温度显示

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,3 +21,7 @@ sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/
 sed -i "s/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g" package/base-files/files/bin/config_generate
 sed -i "s/2.openwrt.pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_generate
 sed -i "s/3.openwrt.pool.ntp.org/s1e.time.edu.cn/g" package/base-files/files/bin/config_generate
+
+# 添加核心温度的显示
+sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
