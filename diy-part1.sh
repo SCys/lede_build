e82544c8b29b16aacd5b543a5114dd33c6053313
SCys <me@iscys.com>
2021-08-14 18:36:17 +0800

    * 更新配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,6 @@
 #
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Other customizations
 mkdir package/custom
@@ -23,6 +19,9 @@ mkdir package/custom
 git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/custom/luci-app-argon-config
 
+# TODO Redmi AC2100 SPEC config
 ## MTK Driver
 ### Include driver and luci-app-mtwifi
-git clone https://github.com/MeIsReallyBa/Redmi2100-WIFI-ProprietaryDriver-linux5.4 package/custom/mtkdriver
+# git clone https://github.com/MeIsReallyBa/Redmi2100-WIFI-ProprietaryDriver-linux5.4 package/custom/mtkdriver
+
+git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
--- diy-part2.sh
+++ diy-part2.sh
@@ -13,14 +13,16 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
+
 # Modify theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-netgear/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify NTP Server
 sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
 sed -i "s/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g" package/base-files/files/bin/config_generate
 sed -i "s/2.openwrt.pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_generate
-sed -i "s/3.openwrt.pool.ntp.org/s1e.time.edu.cn/g" package/base-files/files/bin/config_generate
 
 # 添加核心温度的显示
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
